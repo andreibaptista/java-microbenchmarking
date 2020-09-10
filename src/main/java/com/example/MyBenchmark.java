7cb9df03bcfe9d72808ecae31a0f6a2e0815382b
Andrei Baptista <andreibaptista05@gmail.com>
2020-09-09 22:22:10 -0300

    feat: changing scope to thead scope

--- src/main/java/com/example/MyBenchmark.java
+++ src/main/java/com/example/MyBenchmark.java
@@ -1,28 +1,32 @@
 package com.example;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
-    static final int COUNT = 10000;
+
+    @State(Scope.Thread)
+    public static class ClassState {
+        static final int COUNT = 10000;
+    }
+
+
+
     @BenchmarkMode(Mode.Throughput) @OutputTimeUnit(TimeUnit.NANOSECONDS)
     public List<Boolean> testFillEmptyList() {
         List<Boolean> list = new ArrayList<>();
-        for (int i=0;i<COUNT;i++) {
+        for (int i=0;i<ClassState.COUNT;i++) {
             list.add(Boolean.TRUE);
         }
         return list;
     }
     @Benchmark
     public List<Boolean> testFillAllocatedList() {
-        List<Boolean> list = new ArrayList<>(COUNT);
-        for (int i=0;i<COUNT;i++) {
+        List<Boolean> list = new ArrayList<>(ClassState.COUNT);
+        for (int i=0;i<ClassState.COUNT;i++) {
             list.add(Boolean.TRUE);
         }
         return list;
