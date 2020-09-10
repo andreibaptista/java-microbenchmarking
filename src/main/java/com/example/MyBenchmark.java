571c1b8998658192985b4974a5b0bc2151deb87d
Andrei Baptista <andreibaptista05@gmail.com>
2020-09-09 22:16:27 -0300

    feat: add some benchmark mode - Throughput

--- src/main/java/com/example/MyBenchmark.java
+++ src/main/java/com/example/MyBenchmark.java
@@ -1,13 +1,17 @@
 package com.example;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MyBenchmark {
     static final int COUNT = 10000;
-    @Benchmark
+    @BenchmarkMode(Mode.Throughput) @OutputTimeUnit(TimeUnit.MINUTES)
     public List<Boolean> testFillEmptyList() {
         List<Boolean> list = new ArrayList<>();
         for (int i=0;i<COUNT;i++) {
